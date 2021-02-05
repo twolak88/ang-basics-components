57a5fede64894d1debfe02be133bccc61c583145
twolak88 <twolak88@gmail.com>
2021-02-05 21:49:33 +0100

    styles and selectors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  //styleUrls: ['./app.component.css']
+  styles: [`
+    h3 {
+      color: brown;
+      background-color: yellow;
+    }
+  `]
 })
 export class AppComponent {
 }
