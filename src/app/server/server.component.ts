0ab43b2ba8e9622a1f1447090e288973d8c2707f
twolak88 <twolak88@gmail.com>
2021-02-06 12:00:28 +0100

    server refactoring; using ngClass to apply style

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -2,7 +2,15 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-server',
-  templateUrl: './server.component.html'
+  templateUrl: './server.component.html',
+  styles: [`
+    .online {
+      color: darkgreen
+    }
+    .offline {
+      color: red
+    }
+  `]
 })
 export class ServerComponent {
   serverId: number = 2;
@@ -12,12 +20,16 @@ export class ServerComponent {
     this.serverStatus = Math.random() > 0.5 ? ServerStatus.Online : ServerStatus.Offline;
   }
   
-  getServerStatus() {
-    return this.serverStatus;
+  getServerStatusString() {
+    return this.serverStatus == ServerStatus.Online ? 'online' : 'offline';
   }
   
   getColor() {
     return this.serverStatus == ServerStatus.Online ? 'lightgreen' : 'pink';
+  }
+  
+  isOnline() {
+    return this.serverStatus == ServerStatus.Online;
   }
 }
 enum ServerStatus {
