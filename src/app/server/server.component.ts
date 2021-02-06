c454c4f2a62b912ad087a8e29fabec10a40b17e2
twolak88 <twolak88@gmail.com>
2021-02-06 01:13:28 +0100

    Forms and data bindings

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -5,5 +5,10 @@ import { Component } from '@angular/core';
   templateUrl: './server.component.html'
 })
 export class ServerComponent {
-
+  serverId: number = 2;
+  serverStatus: string = 'offline';
+  
+  getServerStatus() {
+    return this.serverStatus;
+  }
 }
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -4,16 +4,60 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-servers',
   //selector: '[app-servers]',
   //selector: '.app-servers',
-  //templateUrl: './servers.component.html',
-  template: `<app-server></app-server>
-            <app-server></app-server>`,
+  templateUrl: './servers.component.html',
+  //template: `<app-server></app-server>
+  //          <app-server></app-server>`,
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
-
-  constructor() { }
+  readonly DEFAULT_NAME = 'InitialName';
+  readonly DEFAULT_HOST = 'localhost';
+  readonly DEFAULT_PORT = '8080';
+  
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created!';
+  serverCreated = false;
+  serverName = this.DEFAULT_NAME;
+  serverHost = this.DEFAULT_HOST;
+  serverPort = this.DEFAULT_PORT;
+  
+  constructor() {
+    setTimeout(()=> {
+      this.allowNewServer = true;
+    }, 2000);
+  }
 
   ngOnInit(): void {
   }
-
+  
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was created! Server name is ' + this.serverName;
+    this.serverCreated = true;
+    this.allowNewServer = false;
+    //console.log("Server Created: " + this.serverCreated);
+  }
+  
+  onMouseOver() {
+    this.serverCreationStatus = 'Are you sure you can create Server with name: ' + this.serverName + '?';
+  }
+  
+  onMouseOut() {
+    if (!this.serverCreated) {
+      this.serverCreationStatus = "No server was created!";
+    } else {
+      this.serverCreationStatus = "Server was created! Server name is " + this.serverName;
+    }
+  }
+  
+  onUpdateServerName(event: Event){
+    console.log(event);
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
+  
+  resetControls() {
+    this.serverName = this.DEFAULT_NAME;
+    this.serverHost = this.DEFAULT_HOST;
+    this.serverPort = this.DEFAULT_PORT;
+  }
+ 
 }
